6f21168e620a6a3c827fc9d2ce29f28a639b8512
Alexander Demchenko <a.demchenko@aldem.ru>
2017-11-29 15:39:16 +0200

    fix phpunit

--- tests/Unit/Controller/PageControllerTest.php
+++ tests/Unit/Controller/PageControllerTest.php
@@ -7,27 +7,27 @@ use OCP\AppFramework\Http\TemplateResponse;
 use OCA\FractalNote\Controller\PageController;
 use OCA\FractalNote\Service\ProviderFactory;
 
-
 class PageControllerTest extends PHPUnit_Framework_TestCase {
 	private $controller;
 	private $userId = 'john';
 
-	public function setUp() {
-		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+	public function setUp()
+    {
+        $request = $this->getMockBuilder('OCP\IRequest')->getMock();
+        $mockedProvider = $this->getMockBuilder('OCA\FractalNote\Service\NotesStructure')->getMock();
         $providerFactory = $this->getMockBuilder('OCA\FractalNote\Service\ProviderFactory')
             ->disableOriginalConstructor()
-            ->getMock();
+            ->getMock()
+                ->expects($request)->method('createProviderByRequest')->willReturn($mockedProvider);
 
-		$this->controller = new PageController(
-			'fractalnote', $request, $this->userId, $providerFactory
-		);
+		$this->controller = new PageController('fractalnote', $request, $this->userId, $providerFactory);
 	}
 
-	public function testIndex() {
+	public function testIndex()
+    {
 		$result = $this->controller->index();
 
 		$this->assertEquals('404', $result->getTemplateName());
 		$this->assertTrue($result instanceof TemplateResponse);
 	}
-
 }
